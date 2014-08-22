9f4366190bcf87f82f161c0e80ae6ad6918c888a
Jeff Jacob <jeff.jacob@nanaimo.ca>
2014-08-21 17:25:29 -0700

    Nightly checkin (unstable)

---
+++ Open511DotNet/GMLPolygon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Open511DotNet
+{
+    public class GmlPolygon : GmlBase
+    {
+        
+        [XmlElement("exterior")]
+        public GmlRing Exterior { get; set; }
+
+        [XmlElement("interior")]
+        public GmlRing Interior { get; set; }
+    }
+}
